6da3496b76c01af1d2754b343858cdf052ec18a3
nanotoly <dong991207@unist.ac.kr>
2020-05-20 10:07:27 +0900

    format change

--- insole/insole.ino
+++ insole/insole.ino
@@ -1,11 +1,11 @@
-//int insole_type = 0; //left insole
-int insole_type = 1; //right insole
-
+  
+int insole_type = 0; //left insole
+//int insole_type = 1; //right insole
 
 #include "BluetoothSerial.h"
 #include <ArduinoJson.h>
 using namespace std;
- 
+int col_s[4] = {8,9,10,11};
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 
@@ -14,48 +14,49 @@ using namespace std;
 #endif
 
  
+void Select(int ch_num){
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(col_s[i], bitRead(ch_num, i));
+  }
+}
+
+int getMuxData(int ch){
+  Select(ch);
+  return analogRead(A0);
+}
 
 BluetoothSerial SerialBT;
 
+  float test[9];
   float P[9];
   float C[2];
   float T[5];
 
+
 //덤프 데이터 생성
 void dump(){
-for(int i=0; i<9;i++  ){
-if(i<2){
-  C[i]=i;
-
-}
-if(i<5){
-  T[i]=i;
-
-}
-P[i]=i;
-
-}
+  for(int i=0; i<9;i++  ){
+    if(i<2){
+      C[i]=i;
+    }
+    if(i<5){
+      T[i]=i;
+    }
+    P[i]=i;
+  }
 }
  String jsondata = "";
     
     StaticJsonDocument <1500> doc;
-    // DynamicJsonDocument doc(1024); 동적json 생성
-    JsonObject root = doc.to<JsonObject>();
-
-    JsonArray Type = root.createNestedArray("Type");
-    JsonArray Pressure = root.createNestedArray("Pressure");
-    JsonArray Temper =root.createNestedArray("Temper");
-    JsonArray Cop =root.createNestedArray("Cop");
-
+    
+    JsonObject Pressure = doc.createNestedObject("Pressure");
+    JsonObject Cop =doc.createNestedObject("Cop");
+    JsonObject Temper =doc.createNestedObject("Temper");
 
 void setup() {
+
+  Serial.begin(115200);
   
-  
-
-   
-
-  Serial.begin(9600);
-
   if(insole_type == 0){
     SerialBT.begin("left insole"); //Bluetooth device name
   }
@@ -71,35 +72,63 @@ void setup() {
 
 }
 
- 
+  
 
 boolean bToogle = 0;
 
 void loop() {
-
+  if(insole_type== 0){
+    doc["type"] ="left";
+  }
+  else if(insole_type== 1){
+    doc["type"] ="right";
+  }
         //  값 삽입
-      for(int i=0; i<9;i++  ){
-      if(i<2){
-        Cop[i]=C[i];
+  for(int i=0; i<9;i++  ){
 
+    Select(i);
+    if(i==0){
+      Cop["x"]=C[i];
+
+    }
+    if(i==1){
+      Cop["y"]=C[i];
+
+    }
+    if(i<5){
+      String T_;
+      if(insole_type == 0){
+        T_ = "TL";
+        T_+=i;
       }
-      if(i<5){
-        Temper[i]=T[i];
-
+      else if (insole_type == 1){
+        T_ = "TR";
+        T_+=i;
       }
-      Pressure[i]=P[i];
+      Temper[T_]=T[i];
 
-      }
-      Type[0] = insole_type;
-
+    }
+    String P_;
+    if(insole_type == 0){
+      P_ = "PL";
+      P_+=i;
+    }
+    else if (insole_type == 1){
+      P_ = "PR";
+      P_+=i;
+    }
+    
+    
+    Pressure[P_]=P[i];
+    //Serial.println(i);
+  }
 
 //      serializeJsonPretty(doc, jsondata); //JSON 이쁘게 보이는거
       // serializeJson(doc, jsondata); // JSON 노말하게 보이는것
       
-  serializeJson(doc, Serial);
+  
+  serializeJsonPretty(doc, Serial);
   Serial.println(" ");
-
-      
 
 
   if (SerialBT.available()) {
@@ -107,13 +136,13 @@ void loop() {
     Serial.println("send");
 //    SerialBT.write(String(jsondata));
 
-  }
-
- 
-
- 
-
   
+
+ 
+
+ 
+
+  }
 
   delay(1000);
 
